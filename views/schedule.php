2fcf260ea73118d496f2fc19c9420735739bf637
Arman Hastings <armanhastings@gmail.com>
2017-07-31 12:25:23 -0700

    Adding menu selections for 24hours.

--- views/schedule.php
+++ views/schedule.php
@@ -26,25 +26,36 @@ $buttons = array(
 //    anchor_custom('/app/bmbackup/schedule/backup', lang('bmbackup_backup'), 'high')
 );
 
-$times = array();
 $times[24] = 'Disabled';
+
+//AM Hours
+$times[0] = '12:00 AM';
+$times[1] = '01:00 AM';
+$times[2] = '02:00 AM';
+$times[3] = '03:00 AM';
+$times[4] = '04:00 AM';
+$times[5] = '05:00 AM';
+$times[6] = '06:00 AM';
+$times[7] = '07:00 AM';
+$times[8] = '08:00 AM';
+$times[9] = '09:00 AM';
+$times[10] = '10:00 AM';
+$times[11] = '11:00 AM';
+
+//PM Hours
+$times[12] = '12:00 PM';
+$times[13] = '01:00 PM';
+$times[14] = '02:00 PM';
+
+$times[15] = '03:00 PM';
+$times[16] = '04:00 PM';
+$times[17] = '05:00 PM';
 $times[18] = '06:00 PM';
 $times[19] = '07:00 PM';
 $times[20] = '08:00 PM';
 $times[21] = '09:00 PM';
 $times[22] = '10:00 PM';
 $times[23] = '11:00 PM';
-$times[0] = '12:00 AM';
-$times[1] = '01:00 AM';
-$times[2] = '02:00 AM';
-// test times for troubleshooting
-$times[10] = '10:00 AM';
-$times[11] = '11:00 AM';
-$times[12] = '12:00 PM';
-$times[13] = '01:00 PM';
-$times[14] = '02:00 PM';
-
-
     
 ///////////////////////////////////////////////////////////////////////////////
 // Form
